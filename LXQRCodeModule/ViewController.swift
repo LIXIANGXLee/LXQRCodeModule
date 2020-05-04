8dbee6f8ae41bc5e70a5a76676ec8ff66b0a72aa
lixiang <lixiang@gokuaidian.com>
2020-05-04 17:10:36 +0800

    Initial Commit

---
+++ LXQRCodeModule/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LXQRCodeModule
+//
+//  Created by Mac on 2020/5/4.
+//  Copyright © 2020 李响. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
